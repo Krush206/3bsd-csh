6a8c047f02c0451e25b13ff0e307996ef2678e54
Krush206 <37114863+Krush206@users.noreply.github.com>
2023-12-02 13:20:57 -0300

    func.c: assign NULL after deallocating fargv

--- func.c
+++ func.c
@@ -1497,7 +1497,9 @@ dofunction(Char **v, struct command *t)
 	if (fargv->prev) {
 	    fargv = fargv->prev;
 	    free(fargv->next);
-	} else
+	} else {
 	    free(fargv);
+	    fargv = NULL;
+	}
     }
 }
