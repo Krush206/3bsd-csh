3885172b44dedbfb503ffb022e8ab1a37d061134
Matheus Garcia <tateusg@protonmail.com>
2026-09-20 20:58:03 -0300

    dol.c

--- dol.c
+++ dol.c
@@ -33,9 +33,15 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <errno.h>
+#ifdef __linux__
+#include <bsd/stdlib.h>
+#include <bsd/string.h>
+#include <bsd/unistd.h>
+#else
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#endif
 #include <stdarg.h>
 
 #include "csh.h"
