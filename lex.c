f9af6b25654b24bb58f1a13d63e41670b23e6e9b
Matheus Garcia <tateusg@protonmail.com>
2026-09-20 20:58:03 -0300

    lex.c

--- lex.c
+++ lex.c
@@ -34,9 +34,15 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <errno.h>
+#ifdef __linux__
+#include <bsd/stdlib.h>
+#include <bsd/string.h>
+#include <bsd/unistd.h>
+#else
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#endif
 #include <stdarg.h>
 
 #include "csh.h"
