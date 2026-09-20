29b0f625698956ee87f08208c7c5b8c0904ca940
Matheus Garcia <tateusg@protonmail.com>
2026-09-20 20:58:03 -0300

    misc.c

--- misc.c
+++ misc.c
@@ -31,8 +31,13 @@
  */
 
 #include <sys/param.h>
+#ifdef __linux__
+#include <bsd/stdlib.h>
+#include <bsd/unistd.h>
+#else
 #include <stdlib.h>
 #include <unistd.h>
+#endif
 #include <stdarg.h>
 
 #include "csh.h"
